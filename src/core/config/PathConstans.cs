545e683bcc6423403e625b6fe82c578a60d6a4a0
xXrederXx <morgenthalerthierry@gmail.com>
2026-06-07 13:37:24 +0200

    feat(core): add path constants

---
+++ src/core/config/PathConstans.cs
@@ -0,0 +1,7 @@
+namespace QuickApi.Core.Config;
+
+public static class PathConstants
+{
+    public const string EntitiesFolderName = "entities";
+    public const string EndpointsFolderName = "endpoints";
+}
