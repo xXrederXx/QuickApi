6cfac52c8afc24977afae7b9f3158e793f1b86fc
xXrederXx <morgenthalerthierry@gmail.com>
2026-06-07 20:07:57 +0200

    feat(core): add file name regristry

---
+++ src/core/common/FileNameRegristry.cs
@@ -0,0 +1,32 @@
+using System.Collections.Concurrent;
+
+// This class is form my other project "YSharp" and only has minimal ajustments
+// https://github.com/xXrederXx/YSharp/blob/main/src/Util/FileNameRegistry.cs
+
+namespace QuickApi.Core.Common;
+
+public static class FileNameRegistry
+{
+    private static readonly string[] _idToName = new string[256];
+    private static readonly ConcurrentDictionary<string, byte> _nameToId = new();
+    private static int _nextId = 1; // 0 = null
+
+    public static byte GetFileId(string name) => _nameToId.GetOrAdd(name, AllocateId);
+
+    public static string GetFileName(byte id) => _idToName[id] ?? string.Empty;
+
+    private static byte AllocateId(string name)
+    {
+        // This method is only called when the name does not yet exist.
+        int id = Interlocked.Increment(ref _nextId);
+        if (id > byte.MaxValue)
+            throw new InvalidOperationException("File ID overflow (256 max)");
+
+        byte b = (byte)id;
+
+        // Write to shared array safely – this is a single atomic pointer write
+        _idToName[b] = name;
+
+        return b;
+    }
+}
