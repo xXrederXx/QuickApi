34017778e45676b2f10a5511721d4da1fe0ab013
xXrederXx <morgenthalerthierry@gmail.com>
2026-06-07 12:35:22 +0200

    feat(cli): add new command

--- src/cli/Program.cs
+++ src/cli/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        RootCommand rootCommand = new RootCommand("The CLI Interface for Quick API");
+
+        AddNewCommand(rootCommand);
+        
+        rootCommand.Parse(args).Invoke();
+    }
+
+    static void AddNewCommand(RootCommand rootCommand)
+    {
+        Command command = new Command("new", "Create a new Quick API Project");
+
+        var path = new Argument<string>("path")
+        {
+            Description = "Where to create the Project",
+            DefaultValueFactory = _ => "."
+        };
+
+        command.SetAction(pr =>
+        {
+           System.Console.WriteLine(pr.GetValue<string>("path")); 
+           System.Console.WriteLine(Path.GetFullPath(pr.GetValue<string>("path")));
+        });
+
+        command.Arguments.Add(path);
+
+        rootCommand.Add(command);
+    }
+}
