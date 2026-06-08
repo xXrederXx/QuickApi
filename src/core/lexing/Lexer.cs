5190127d552ea6f71c9f6c262de9ae492dc0d523
xXrederXx <morgenthalerthierry@gmail.com>
2026-06-08 08:06:43 +0200

    feat(core): add lexing

--- src/core/lexing/Lexer.cs
+++ src/core/lexing/Lexer.cs
@@ -1,3 +1,4 @@
+using FastEnumUtility;
 using QuickApi.Core.Common;
 
 namespace QuickApi.Core.Lexing;
@@ -7,8 +8,7 @@ public class Lexer
     private const char StopChar = char.MaxValue;
     private readonly string Text;
     private Position Pos;
-    private char currentChar => Pos.Index < Text.Length ? Text[Pos.Index] : StopChar;
-
+    private char currentChar;
 
     public Lexer(string path)
     {
@@ -20,6 +20,49 @@ public class Lexer
 
     public Result<BaseToken[], string> GetTokens()
     {
-        return Result<BaseToken[], string>.Fail("Not Implemented");
+        List<BaseToken> tokens = [];
+        while (currentChar is not StopChar)
+        {
+            if (currentChar is ':')
+            {
+                tokens.Add(new BaseToken(Pos, TokenType.COLON));
+                Advance();
+            }
+            else if (char.IsLetter(currentChar))
+            {
+                tokens.Add(MakeIdentifier());
+            }
+            else
+            {
+                return Result<BaseToken[], string>.Fail("Invalid Char: " + currentChar);
+
+            }
+        }
+        tokens.Add(new BaseToken(Pos, TokenType.EOF));
+        return Result<BaseToken[], string>.Success(tokens.ToArray());
+    }
+
+    private BaseToken MakeIdentifier()
+    {
+        string identifier = string.Empty;
+        Position startPos = Pos;
+        while (IsValidIdentifierChar(currentChar))
+        {
+            identifier += currentChar;
+            Advance();
+        }
+        bool IsKeyword = FastEnum.TryParse(identifier, out KeywordType keywordType);
+
+        return IsKeyword
+            ? new Token<KeywordType>(startPos, TokenType.KEYWORD, keywordType)
+            : new Token<string>(startPos, TokenType.IDENTIFIER, identifier);
+    }
+
+    private static bool IsValidIdentifierChar(char c) => char.IsLetterOrDigit(c) || c == '_';
+
+    private void Advance()
+    {
+        Pos = Pos.Advance(currentChar);
+        currentChar = Pos.Index < Text.Length ? Text[Pos.Index] : StopChar;
     }
 }
