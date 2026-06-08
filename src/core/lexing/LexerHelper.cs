9ee30f2c974e837a1109c09878023983b30192ca
xXrederXx <morgenthalerthierry@gmail.com>
2026-06-08 14:58:32 +0200

    feat(core): add more single types and make dict immutable

--- src/core/lexing/LexerHelper.cs
+++ src/core/lexing/LexerHelper.cs
@@ -1,13 +1,18 @@
+using System.Collections.Immutable;
+
 namespace QuickApi.Core.Lexing;
 
 public static class LexerHelper
 {
-    private static readonly Dictionary<char, TokenType> SingleMap = new Dictionary<char, TokenType>() {
+    public static readonly ImmutableDictionary<char, TokenType> SingleMap = new Dictionary<char, TokenType>() {
         {':', TokenType.COLON},
         {'/', TokenType.SLASH},
         {'{', TokenType.CURLY_LEFT},
-        {'}', TokenType.CURLY_RIGHT}
-    };
+        {'}', TokenType.CURLY_RIGHT},
+        {'\n', TokenType.NEWLINE},
+        {'\r', TokenType.NEWLINE},
+        {'\t', TokenType.TAB},
+    }.ToImmutableDictionary();
 
     public static bool TryParseSingle(char input, out TokenType type)
         => SingleMap.TryGetValue(input, out type);
