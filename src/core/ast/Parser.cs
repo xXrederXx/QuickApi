59dcd30f55600627665aaa6f21b2b4db8ea533ce
xXrederXx <morgenthalerthierry@gmail.com>
2026-06-08 19:03:01 +0200

    feat(core): start parser functions

--- src/core/ast/Parser.cs
+++ src/core/ast/Parser.cs
@@ -1,3 +1,5 @@
+using QuickApi.Core.Ast.Nodes;
+using QuickApi.Core.Common;
 using QuickApi.Core.Lexing;
 
 namespace QuickApi.Core.Ast;
@@ -5,17 +7,73 @@ namespace QuickApi.Core.Ast;
 public class Parser
 {
     private BaseToken[] Tokens { get; init; }
+    private BaseToken currentToken;
+    private int currentIndex = -1;
 
     public Parser(BaseToken[] tokens)
     {
         Tokens = tokens;
     }
 
-    private int currentIndex = -1;
+    public void ParseTokens() { }
 
-    
-    public static void ParseTokens()
+    private Result<EndpointNode, string> ParseEndpoint()
     {
-        
+        if (
+            currentToken.type is not TokenType.HTTPMETHOD
+            || currentToken is not Token<HttpMethodType> methodTok
+        )
+        {
+            return Result<EndpointNode, string>.Fail("Token must be of type HTTP METHOD");
+        }
+        Advance();
+        return Result<EndpointNode, string>.Fail("Not Implemented");
+    }
+
+    private Result<PathNode, string> ParsePath()
+    {
+        List<PathElement> elements = [];
+        while (currentToken.type is not TokenType.NEWLINE or TokenType.EOF)
+        {
+            if (currentToken.type is TokenType.SLASH)
+            {
+                Advance();
+                continue;
+            }
+
+            if (currentToken.type is TokenType.CURLY_LEFT)
+            {
+                Advance();
+                if (
+                    currentToken.type is not TokenType.IDENTIFIER
+                    || currentToken is not Token<string> identifierTok
+                )
+                {
+                    return Result<PathNode, string>.Fail("Token must be of type Identifier");
+                }
+                Advance();
+                // symbol }
+                Advance();
+
+                elements.Add(
+                    new PathElement()
+                    {
+                        Type = PathElement.PathElementType.Variable,
+                        Identifier = identifierTok,
+                    }
+                );
+                continue;
+            }
+        }
+        return Result<PathNode, string>.Fail("Not Implemented");
+    }
+
+    private void Advance()
+    {
+        currentIndex++;
+        if (currentIndex < Tokens.Length)
+        {
+            currentToken = Tokens[currentIndex];
+        }
     }
 }
