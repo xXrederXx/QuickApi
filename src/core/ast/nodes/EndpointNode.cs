cb28d682fe92d0ca7afcf98c49b5fc0a80010e18
xXrederXx <morgenthalerthierry@gmail.com>
2026-06-09 19:33:11 +0200

    feat(core): add requiered keyword for endpoint node

--- src/core/ast/nodes/EndpointNode.cs
+++ src/core/ast/nodes/EndpointNode.cs
@@ -5,9 +5,12 @@ namespace QuickApi.Core.Ast.Nodes;
 
 public class EndpointNode
 {
-    public Position StartPos { get; init; }
+    public Position StartPos
+    {
+        get => MethodToken.StartPosition;
+    }
 
-    public Token<HttpMethodType> MethodToken { get; init; }
-    public PathNode PathNode { get; init; }
-    public EndpointAttribute[] Attributes { get; init; }
+    public required Token<HttpMethodType> MethodToken { get; init; }
+    public required PathNode PathNode { get; init; }
+    public required EndpointAttribute[] Attributes { get; init; }
 }
